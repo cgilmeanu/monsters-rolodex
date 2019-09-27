c9d61948b0d638adf3e14eac9da8d5a79bdd76f0
Claudiu Gilmeanu <cgilmeanu@gmail.com>
2019-09-27 12:30:58 +0300

    Updates

---
+++ precache-manifest.d46e764808ec181083be17c38a304dff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f1e9fd56b954fe16744e9a25215ec97",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "ab42aa9b3aaec8a15e74",
+    "url": "/monster-rolodex/static/css/main.e1b4e944.chunk.css"
+  },
+  {
+    "revision": "46012db1d69cedf8d1ab",
+    "url": "/monster-rolodex/static/js/2.3d8af1b9.chunk.js"
+  },
+  {
+    "revision": "ab42aa9b3aaec8a15e74",
+    "url": "/monster-rolodex/static/js/main.dd8b6fff.chunk.js"
+  },
+  {
+    "revision": "52b3135f1aa77ef4ddba",
+    "url": "/monster-rolodex/static/js/runtime~main.b7028ca9.js"
+  }
+]);
